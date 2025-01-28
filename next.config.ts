8cfe61be24e4e1803e51534003171acf77b9153d
delfin <delfinjfb@gmail.com>
2025-01-28 21:02:32 +0100

    feat: configure image domains for Contentful assets

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import { i18n } from './next-i18next.config';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   i18n,
+  images: {
+    domains: ['images.ctfassets.net'],
+  },
 };
 
 export default nextConfig;
